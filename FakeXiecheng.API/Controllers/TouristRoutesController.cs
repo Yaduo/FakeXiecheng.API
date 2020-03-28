7c8e2b9cfce23a2cf4dc3da0b64591cc16715543
Yaduo Liu <yaduol@hotmail.com>
2020-03-28 10:36:20 -0700

    get tourist routes data

---
+++ FakeXiecheng.API/Controllers/TouristRoutesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FakeXiecheng.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TouristRoutesController : Controller //ControllerBase
+    {
+        private ITouristRouteRepository _touristRouteRepository;
+
+        public TouristRoutesController(ITouristRouteRepository touristRouteRepository)
+        {
+            _touristRouteRepository = touristRouteRepository ??
+                throw new ArgumentNullException(nameof(touristRouteRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetTouristRoutes()
+        {
+            var touristRoutes = _touristRouteRepository.GetAllTouristRoutes();
+            return new JsonResult(touristRoutes);
+        }
+    }
+}
