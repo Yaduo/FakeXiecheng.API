cd588ef022f4edce8c7ac60f43d20fdf3b130f77
Yaduo Liu <yaduol@hotmail.com>
2020-03-30 12:41:51 -0700

    Validating Input when Updating a Resource with PUT (part 1)

--- FakeXiecheng.API/Dtos/TouristRouteForUpdateDto.cs
+++ FakeXiecheng.API/Dtos/TouristRouteForUpdateDto.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using FakeXiecheng.API.ValidationAttributes;
+
 namespace FakeXiecheng.API.Dtos
 {
+    [TouristRouteTitleMustBeDifferentFromDescriptionAttribute(
+          ErrorMessage = "Title must be different from description.")]
     public class TouristRouteForUpdateDto
     {
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public string Description { get; set; }
         public decimal OriginalPrice { get; set; }
         public double? DiscountPercent { get; set; }
