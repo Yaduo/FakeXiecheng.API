71de044dac44544504c8cf150ee9428f2b310e8e
Yaduo Liu <yaduol@hotmail.com>
2020-04-01 13:48:39 -0700

    add authentication helper for throwing 401 in api

---
+++ FakeXiecheng.API/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace FakeXiecheng.API.Helpers
+{
+    public static class AuthenticationHelper
+    {
+        public static Func<RedirectContext<CookieAuthenticationOptions>, Task> CookieAuthReplaceRedirector(
+            HttpStatusCode statusCode,
+            Func<RedirectContext<CookieAuthenticationOptions>, Task> existingRedirector
+        )
+        => context =>
+        {
+            if (context.Request.Path.StartsWithSegments("/api"))
+            {
+                context.Response.StatusCode = (int)statusCode;
+                return Task.CompletedTask;
+            }
+            return existingRedirector(context);
+        };
+    }
+}
